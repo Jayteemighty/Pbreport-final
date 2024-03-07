83186f697e32cfbbed8cb2da1a909c6d2afdf2ab
Adesina Joshua <tolujosh1@gmail.com>
2024-03-07 20:40:42 +0100

    corrections for urls configurations

--- project/urls.py
+++ project/urls.py
@@ -4,27 +4,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import permissions
-from drf_spectacular.views import  SpectacularAPIView, SpectacularSwaggerView
+
 
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-...
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+from drf_spectacular.views import  SpectacularAPIView, SpectacularSwaggerView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -45,9 +32,8 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
