f6caaa06e7447eb32a93a6095f92e8f5323ee0c1
Adesina Joshua <tolujosh1@gmail.com>
2024-03-07 01:01:16 +0100

    feature: creaate functions for paystack checkout

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,52 @@
+import requests
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status, authentication, permissions
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, MyOrderSerializer
+
+#from rest_framework.response import Response
+#from drf_spectacular.utils import extend_schema
+
+
+api_key = settings.PAYSTACK_SECRET_KEY
+url = settings.PAYSTACK_INITIALIZE_PAYMENT_URL
+@api_view(['POST'])
+@authentication_classes([authentication.TokenAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def checkout(request):
+    serializer = OrderSerializer(data=request.data)
+
+    if serializer.is_valid():
+        # Calculate the total paid amount
+        paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
+
+        try:
+            # Call the verify_payment method to verify the payment using Paystack API
+            if request.user.order.verify_payment(serializer.validated_data['reference']):
+                serializer.save(user=request.user, paid_amount=paid_amount)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({'error': 'Payment verification failed'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class OrdersList(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        orders = Order.objects.filter(user=request.user)
+        serializer = MyOrderSerializer(orders, many=True)
+        return Response(serializer.data)
