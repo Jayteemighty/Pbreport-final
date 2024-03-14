794cb77aa455cb39159f9c1b7ca44d6b0a6bcc37
Adesina Joshua <tolujosh1@gmail.com>
2024-03-14 16:33:14 +0100

    adjustments for models.py

--- products/models.py
+++ products/models.py
@@ -5,21 +5,21 @@ from django.core.files import File
 from django.db import models
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     slug = models.SlugField()
 
     class Meta:
-        ordering = ('title',)
+        ordering = ('name',)
     
     def __str__(self):
-        return self.title
+        return self.name
     
     def get_absolute_url(self):
         return f'/{self.slug}/'
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -36,7 +36,7 @@ class Product(models.Model):
         ordering = ('-date_added',)
     
     def __str__(self):
-        return self.title
+        return self.name
     
     #def get_absolute_url(self):
     #    return f'/{self.category.slug}/{self.slug}/'
@@ -66,10 +66,10 @@ class Product(models.Model):
         thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
 
-        thumbnail = File(thumb_io, name=image.title)
+        thumbnail = File(thumb_io, name=image.name)
 
         return thumbnail
     
 class PDFDocument(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     pdf_file = models.FileField(upload_to='pdf_files/')
